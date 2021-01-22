ea0bc516cbd54c54a676e9396e1c43a1923e1310
Robinson Manuel Sarante Colon <robinsonsarante1234@gmail.com>
2021-01-22 14:05:40 -0400

    Add project files.

---
+++ Ejercicio2(Potencia de un numero)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//•	Hacer un programa que calcule el resultado de un número elevado a cualquier potencia.
+
+namespace Ejercicio2_Potencia_de_un_numero_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada = "";
+            float num = 0, mult = 0;
+            int exponente = 0;
+
+            Console.WriteLine("Este programa calcula la potencia de un numero\n");
+            Console.Write("Ingrese el numero: ");
+            entrada = Console.ReadLine();
+            num = Convert.ToSingle(entrada);
+
+            Console.Write("Ingrese el exponente: ");
+            entrada = Console.ReadLine();
+            exponente = Convert.ToInt32(entrada);
+
+            mult = num * num;
+            for(int i=2; i<exponente; i++)
+            {
+                mult *= num;
+            }
+
+            Console.WriteLine("Resultado es: {0}", mult);
+        }
+    }
+}
---
+++ Ejercicio3(Promedio_edad)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//•	Hacer un programa que calcule el promedio de edad de un grupo de personas 
+// y diga cuál es la de edad más grande y cuál es la más joven.
+namespace Ejercicio3_Promedio_edad_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
